269156e4f6dece4a20777cde933d429eebb9f924
Swati <swatisom222@gmail.com>
2026-03-28 17:49:44 +0530

    CSS added in app.css and counter.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import Counter from "./components/Counter";
+import './App.css';
 
 function App() {
   return (
     <>
-      <h1>Counter App</h1>
-      <Counter />
+      <div className="container">
+        <div>
+        <h1>Counter App</h1>
+        <Counter />
+        </div>
+      </div>
     </>
   );
 }
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 import useCount from "../hook/useCount";
+import './Counter.css';
 
 function Counter() {
   const { count, increment, decrement, reset } = useCount(0);
   return (
     <div>
       <p>Count : {count} </p>
-      <button onClick={increment}>Increment</button>
-      <button onClick={decrement}>Decrement</button>
-      <button onClick={reset}>Reset</button>
+      <button className="add" onClick={increment}>Increment</button>
+      <button className="minus" onClick={decrement}>Decrement</button>
+      <button className="reset" onClick={reset}>Reset</button>
     </div>
   );
 }
